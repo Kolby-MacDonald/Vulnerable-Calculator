a9d05fa3ae42abb8cb87877e5853092d7e67b088
Kolby MacDonald <115326488+Kolby-MacDonald@users.noreply.github.com>
2023-06-01 12:22:56 -0600

    Overhaul Updates
    Better demonstration of application updates.

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # Vulnerable-Calculator
-Python is not as safe as people believe. This repository demonstrates the eval exploit.
+Python is not as safe as people believe.
+
+Python is not as secure as some may think, eval() demonstrates one of pythons biggest security flaws, command injection. SQL
+controllers, system modules, subprocess functions and even some built-in python functions are not secure against command injection. I built this mini-calculator as a demonstration of a programming mistake that could be fatal to a system.
+
+Another use-case for command injection would be external command injection. For example downloading a simple python calculator that uses eval() and a hidden socket communication to control your PC remotely. Please understand not only what you are doing with your code, but what others can do with your code as well.
+
+--------------------------------------------------------------------------------
+Examples To Use in calc_vuln.py:
+Use the calculator normally, then attempt the following:
+1. __import__('cmdline_loadbar.py').run()
+2. __import__('os').system('start cmd')
+3. __import__('subprocess').Popen('C:\\Windows\\System32\\calc.exe')
+4. __import__('webbrowser').open('https://www.linkedin.com/in/kolby-macdonald/')
+--------------------------------------------------------------------------------
--- calc_vuln.py
+++ calc_vuln.py
@@ -1,24 +1,45 @@
+# --------------------------------------------------------------------------------
+# Name: Kolby MacDonald
+# Project: Vulnerable Calculator 
+# 
+# Purpose:
+#     Python is not as secure as some may think, eval() demonstrates
+# one of pythons biggest security flaws, command injection. SQL
+# controllers, system modules, subprocess functions and even some 
+# built-in python functions are not secure against command injection. 
+# I built this mini-calculator as a demonstration of a programming 
+# mistake that could be fatal to a system.
+#
+#     Another use-case for command injection would be external command
+# injection. For example downloading a simple python calculator that
+# uses eval() and a hidden socket communication to control your PC 
+# remotely. Please understand not only what you are doing with your 
+# code, but what others can do with your code as well.
+# --------------------------------------------------------------------------------
+
+# Import GUI Module
 from PyQt5 import QtWidgets, uic
-from PyQt5.QtWidgets import QMessageBox, QLineEdit
 
-input_string = ''
-output_string = ''
-
+# Update the GUI everytime a conditional change is made.
 def update_screen():
     global input_string, output_string
     gui.input_line_edit.setText(input_string)
     gui.output_line_edit.setText(output_string)
 
+# Update the input string before evaluating an expression.
 def confirm_input():
     global input_string
     input_string = gui.input_line_edit.text()
 
+# Determin which button was pressed and respond accrodingly.
 def pressed_button(button):
     global input_string, output_string
 
+    # For most buttons, we simply just add their value to the expression.
     if (button in range(10)) or button in ['+', '-', '(', ')', '/', '*', '**']:
         input_string += str(button)
 
+    # sign_button changes the expressions sign.
     elif button == 'sign_button':
         try:
             tempList = list(input_string)
@@ -31,48 +52,63 @@ def pressed_button(button):
         except:
             pass
     
-    elif button == ".":
+    # Only add decimals after numbers.
+    elif button == ".": #Could be updated to not allow edge cases. (Eg. 8.7.2).
         tempList = list(input_string)
         if tempList[-1] not in ['+', '-', '(', ')', '/', '*', '**', '.']:
             input_string += button
 
-    
+    # If the button is "=" then evaluate the expression.
     elif button == 'evaluate_button':
         confirm_input()
-        try:
-            output_string = str(eval(input_string))
-            input_string = output_string
+        try: #HERE IS THE SECURITY FLAW - Notice how simple of a mistake this can be.
+            output_string = str(eval(input_string)) # One simple oversight can...
+            input_string = output_string # Completely control your system.
         except:
             pass
     
+    # Percent "%" simply divides by 100 and rounds to two decimal places.
     elif button == 'percent_button':
         try:
             input_string = str(round((float(input_string)/100),2))
             output_string = input_string
         except:
             pass
-
+    
+    # Clear all just resets our input and output string.
     elif button == 'clear_all_button':
         input_string = ''
         output_string = ''
 
+    # Clear Input simply resets the input string.
     elif button == 'clear_input_button':
         input_string = ''      
 
+    # Delete last just removes a character from the input string.
     elif button == 'delete_last_button':
         try:
             input_string = input_string[:-1]
         except:
             pass
     
+    # When the line-edit is manually edited, updated the input string.
     elif button == 'input_edited':
         confirm_input()
 
+    #After every button press, update the gui accordingly.
     update_screen()
 
-app = QtWidgets.QApplication([])
-gui = uic.loadUi(r'C:\Users\deadb\Documents\GitHub\Vulnerable-Calculator\calculator_vuln.ui')
+# Holds the current values for input and output to be displayed.
+input_string = ''
+output_string = ''
 
+# Define the UI and application.
+app = QtWidgets.QApplication([])
+
+#gui = uic.loadUi("calculator_vuln_demovid.ui") #Bigger version for demo purposes.
+gui = uic.loadUi("calculator_vuln.ui")
+
+# Map buttons to their respective funcions.
 gui.input_line_edit.editingFinished.connect(lambda: pressed_button('input_edited'))
 gui.sign_button.clicked.connect(lambda: pressed_button('sign_button'))
 gui.zero_button.clicked.connect(lambda: pressed_button(0))
